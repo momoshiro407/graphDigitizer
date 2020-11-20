ebbb1c17f68ac74bf469617b1abba039fcce5f7a
momoshiro407 <rozabmmsr@gmail.com>
2020-11-20 23:31:10 +0900

    画像読込み・キャンバスへの表示処理を実装

--- src/app/digitizer/digitizer.component.ts
+++ src/app/digitizer/digitizer.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as paper from 'paper';
+import { Raster } from 'paper';
+import { view } from 'paper';
 
 @Component({
   selector: 'app-digitizer',
@@ -6,33 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./digitizer.component.scss']
 })
 export class DigitizerComponent implements OnInit {
+  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>
+  @ViewChild('fileInput', { static: false }) fileInput: ElementRef;
+
   dataSource = [
     { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
-    { n: 1, x: 12345, y: 12345 },
   ];
+
+  file: File = null;
+  imgSrc: string | ArrayBuffer = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    paper.setup(this.canvas.nativeElement);
   }
 
+  onChangeFileInput(event): void {
+    if (event.target.files.length === 0) {
+      this.file = null;
+      this.imgSrc = '';
+      return;
+    }
+
+    const image = new Image();
+    const fileReader = new FileReader();
+    this.file = event.target.files[0];
+    fileReader.onload = () => {
+      this.imgSrc = fileReader.result;
+      image.src = fileReader.result as string;
+      image.onload = () => {
+        // 読み込んだ画像の幅・高さを取得する
+        this.setImageToCanvas(image.naturalWidth, image.naturalHeight);
+      }
+    }
+    fileReader.readAsDataURL(this.file);
+  }
+
+  onClickFileInputButton(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  setImageToCanvas(imageWidth: number, imageHeight: number): void {
+    const raster = new Raster('image');
+    // Rasterオブジェクトの幅と高さを、読み込んだ画像の幅・高さに合わせる
+    raster.width = imageWidth;
+    raster.height = imageHeight;
+    // Rasterオブジェクトの中心をキャンバスの中心に合わせる
+    raster.position = view.center;
+  }
 }
